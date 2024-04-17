4dd72ff7bc7777e77779b21df69ea0067916e23b
Pablo <p.derivas@itecriocuarto.org.ar>
2024-04-16 21:11:46 -0300

    Trabajo a medias

--- toDo.js
+++ toDo.js
@@ -2,16 +2,21 @@ let tareas = []
 
 class Tarea{
     constructor(titulo, fecha, hora, descripcion){
-        this.id = tareas.length() + 1
+        this.id = tareas.length + 1
         this.titulo = titulo
         this.fecha = fecha
         this.hora = hora
         this.descripcion = descripcion
         this.completado = false
+        this.visible = true
     }
 
     completar(){
         this.completado = true
+    }
+
+    eliminar(){
+        this.visible = false
     }
 }
 
@@ -21,13 +26,16 @@ const agregarTarea = (titulo, fecha, hora, descripcion) => {
 }
 
 const borrarTarea = (id) => {
-    tareas.find(tarea => tarea.id = id)
+    var index = tareas.map(function(e) { return e.id; }).indexOf(id);
+    let posTarea = tareas.indexOf(tarea => tarea.id = id)
+    console.log(posTarea)
+    tareas.splice(tareas.indexOf(posTarea, 1))
 }
 
-agregarTarea('Lavar los platos', '18/04/2024', '23:59', 'Sacarle el moho al plato y al tenedor')
-
+for(let x = 0; x < 5; x++){
+    agregarTarea('Lavar los platos', '18/04/2024', '23:59', 'Sacarle el moho al plato y al tenedor')
+}
 console.log(tareas)
-
+borrarTarea(3)
+console.log(tareas)
 let input = 0
-
-tareas.find()
